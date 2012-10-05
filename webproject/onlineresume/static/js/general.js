16648eee2b36a92b5af6bd70ee93859bc4e9cfc4
Marcos Gabriel Alcázar <marcos.alcazar@gmail.com>
2012-10-05 14:24:51 -0300

    Loading message while contact message is being sent

--- webproject/onlineresume/static/js/general.js
+++ webproject/onlineresume/static/js/general.js
@@ -1,15 +1,23 @@
-function go_to_contact(post){
-    if (post==undefined){
-        var method = 'GET';
-    } else {
-        var method = 'POST';
-    }
+function go_to_contact_ajax(method){
     $.ajax('/contact/', {
         type: method,
         data: $("#submitform").serializeArray(),
     }).done(function(response) {
         $("#contact_container").html(response);
     })
+}
+function go_to_contact(post){
+    if (post==undefined){
+        var method = 'GET';
+        go_to_contact_ajax(method);
+    } else {
+        var method = 'POST';
+        $("#submitform").fadeOut('fast', function(){
+            $("#submitloading").fadeIn('fast', function(){
+                go_to_contact_ajax(method);
+            });
+        })
+    }
 }
 $(document).ready(function(){
     go_to_contact();
